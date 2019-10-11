c3ca1a1053cb79ee9a90d9412adeb3835ee535e6
goddl717 <goddl717@naver.com>
2019-10-11 16:00:26 +0900

    화면 전환 및 백

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -17,6 +17,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
          btn = (Button)findViewById(R.id.button3);
+         System.out.println("asdf");
          btn.setOnClickListener(new View.OnClickListener(){
              @Override
              public void onClick(View v) {
